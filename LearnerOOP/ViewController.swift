43cc3c3fd239ee0aea03f60027258bf2cf94e4ec
Kevin Susanto <kevin.stnt@gmail.com>
2019-05-15 12:17:17 +0700

    Initial Commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Kevin Susanto on 15/05/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
