daeafc26793ce3bf300cce2f698eacb4b6a6121a
Kevin Susanto <kevin.stnt@gmail.com>
2019-05-15 14:19:58 +0700

    Add Learner Model

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var learnerNameLabel: UILabel!
+    @IBOutlet var learnerAgeLabel: UILabel!
+    @IBOutlet var learnerGenderLabel: UILabel!
+    
+    var learnerInstance: LearnerModel? // Copy of LearnerModel
+    
+    @IBAction func increaseAgeBtn(_ sender: UIButton) {
+        if let instance = learnerInstance {
+            instance.increaseAge()
+            updateUI()
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        learnerInstance = LearnerModel(nameLearner: "Kevin Susanto", ageLearner: 21, genderLearner: "Male", imageProfileLearner: "")
+        
+        updateUI() //create function
     }
+    
+    func updateUI() {
+        if let instance = learnerInstance{
+            learnerNameLabel.text = instance.name
+            learnerAgeLabel.text = "\(instance.age)"
+            learnerGenderLabel.text = instance.gender
 
+        }
+        
+
+    }
+    
 
 }
 
